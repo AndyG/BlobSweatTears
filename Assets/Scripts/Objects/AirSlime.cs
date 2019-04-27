3565d9967724ff52dec984ed7a6626207cfbb125
Alex <alex@discordapp.com>
2019-04-26 23:15:41 -0700

    blood makes blood

---
+++ Assets/Scripts/Objects/AirSlime.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirSlime : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject groundBloodPrefab;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void KillYourself()
+    {
+        var parent = gameObject;
+        Destroy(parent);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var myX = Mathf.RoundToInt(gameObject.transform.position.x);
+        var myY = Mathf.RoundToInt(gameObject.transform.position.y);
+        SpawnMoreBloodAndDie(myX, myY);
+
+    }
+
+    private void SpawnMoreBloodAndDie(int xPosition, int yPosition)
+    {
+        Instantiate(groundBloodPrefab, new Vector3(xPosition, yPosition, 0), Quaternion.identity);
+        KillYourself();
+    }
+}
