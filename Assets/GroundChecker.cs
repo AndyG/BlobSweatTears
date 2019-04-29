8d3e62abbaa99d78af1ab8e4decd3c85066deb98
AndyG <anderson.garron@gmail.com>
2019-04-28 17:56:54 -0700

    added enemy

---
+++ Assets/GroundChecker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class GroundChecker : MonoBehaviour
+{
+
+  private BoxCollider2D _collider;
+
+  [SerializeField]
+  private float skinWidth = 0.02f;
+
+  [SerializeField]
+  private LayerMask groundLayerMask;
+
+  private float rayDepth;
+
+  void Start()
+  {
+    this._collider = GetComponent<BoxCollider2D>();
+    this.rayDepth = skinWidth * 2;
+  }
+
+  public CollisionInfo GetCollisionInfo()
+  {
+    Vector2 bottomLeftOrigin = new Vector2(_collider.bounds.min.x, _collider.bounds.min.y + skinWidth);
+    Vector2 bottomRightOrigin = new Vector2(_collider.bounds.max.x, _collider.bounds.min.y + skinWidth);
+
+    // cast downward and see if there is no blood beneath us.
+    RaycastHit2D groundLeft = Physics2D.Raycast(bottomLeftOrigin, Vector2.down, rayDepth, groundLayerMask);
+    RaycastHit2D groundRight = Physics2D.Raycast(bottomRightOrigin, Vector2.down, rayDepth, groundLayerMask);
+
+    CollisionInfo info = new CollisionInfo();
+    info.left = groundLeft;
+    info.right = groundRight;
+
+    return info;
+  }
+
+  public struct CollisionInfo
+  {
+    public bool left;
+    public bool right;
+  }
+}
---
+++ Assets/Scripts/Enemies/SlippingEnemy.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(CharacterController2D))]
+public class SlippingEnemy : MonoBehaviour, AnimationManager.AnimationProvider
+{
+
+  private AnimationManager animationManager;
+  private CharacterController2D characterController;
+  private Animator animator;
+  private GroundBloodChecker groundBloodChecker;
+  private GroundChecker groundChecker;
+
+  [SerializeField]
+  private float groundSpeed;
+
+  [SerializeField]
+  private State state;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    this.animator = GetComponent<Animator>();
+    this.characterController = GetComponent<CharacterController2D>();
+    this.animationManager = new AnimationManager(animator, this);
+    this.groundBloodChecker = GetComponentInChildren<GroundBloodChecker>();
+    this.groundChecker = GetComponentInChildren<GroundChecker>();
+    this.state = State.PATROLLING;
+    FaceMovementDirection();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (state == State.PATROLLING)
+    {
+      DoPatrol();
+    }
+
+    animationManager.Update();
+  }
+
+  public void OnSlipFinished()
+  {
+    this.state = State.SLIPPED;
+  }
+
+  public string GetAnimation()
+  {
+    if (state == State.PATROLLING)
+    {
+      return "SlippingEnemyGrounded";
+    }
+    else if (state == State.SLIPPING)
+    {
+      return "SlippingEnemySlipping";
+    }
+    else
+    {
+      return "SlippingEnemySlipped";
+    }
+  }
+
+  private void DoPatrol()
+  {
+    characterController.Move(new Vector3(groundSpeed, 0f, 0f));
+    if (groundBloodChecker.IsGroundBloodActive())
+    {
+      this.state = State.SLIPPING;
+      return;
+    }
+
+    // check if walking into wall.
+    if (groundSpeed > 0 && characterController.collisionState.right || groundSpeed < 0 && characterController.collisionState.left)
+    {
+      TurnAround();
+      return;
+    }
+
+    // check if walked off cliff.
+    GroundChecker.CollisionInfo collisionInfo = groundChecker.GetCollisionInfo();
+    if (groundSpeed < 0 && !collisionInfo.left)
+    {
+      TurnAround();
+    }
+    else if (groundSpeed > 0 && !collisionInfo.right)
+    {
+      TurnAround();
+    }
+  }
+
+  private void TurnAround()
+  {
+    this.groundSpeed *= -1;
+    FaceMovementDirection();
+  }
+
+  private void FaceMovementDirection()
+  {
+    if (groundSpeed >= 0)
+    {
+      this.transform.localScale = new Vector3(1f, 1f, 1f);
+    }
+    else
+    {
+      this.transform.localScale = new Vector3(-1f, 1f, 1f);
+    }
+  }
+
+  enum State
+  {
+    PATROLLING,
+    SLIPPING,
+    SLIPPED
+  }
+}
